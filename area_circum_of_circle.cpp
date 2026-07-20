86adbfc9b5588422b696327c1589e627497ca035
aditinarke25-hue <aditinarke25@gmail.com>
2026-07-20 10:41:53 +0530

    Update area_circum_of_circle.cpp

--- area_circum_of_circle.cpp
+++ area_circum_of_circle.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
 using namespace std;
-int main 
+int main()
 {
-cout<<"Hello World";
+float r;
+cout<<("Enter radius: ");
+cin>>r;
+cout<<"Area ="<< (3.14 * r * r)<<endl;
+cout<<"Circumference ="<<(2 * 3.14 * r);
 }
