9887099fdfd3a5ffb08ca2b0df70ee8ec1f9983f
aditinarke25-hue <aditinarke25@gmail.com>
2026-07-27 15:18:27 +0530

    Add files via upload

---
+++ reverse_no.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+int rev=0;
+int rem;
+cin>>n;
+while(n!=0)
+{
+  rem=n%10;
+  rev=rev*10+rem;
+  n=n/10;
+}
+ 
+cout<<"Reverse no is "<<rev;
+}
